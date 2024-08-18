7786a9a001560bc167b2876824b6150cc2584a71
Sebastian Czech <sebaczech@gmail.com>
2024-08-18 15:11:40 +0200

    Fix pre-commit issues

--- outputs.tf
+++ outputs.tf
@@ -19,7 +19,7 @@ output "vcn_id" {
 }
 
 output "subnet_state" {
-  description = "the state of the subnet"
+  description = "The state of the subnet"
   value       = oci_core_subnet.k8s_subnet.state
 }
 
@@ -34,21 +34,21 @@ output "subnet_cidr" {
 }
 
 output "compute_instances_public_ip" {
-  description = "public IPs of created nodes"
-  value       = ["${oci_core_instance.k8s_node.*.public_ip}"]
+  description = "Public IPs of created nodes"
+  value       = [oci_core_instance.k8s_node[*].public_ip]
 }
 
 output "compute_instances" {
+  description = "Names and IPs of created instances"
   value = {
-    name       = oci_core_instance.k8s_node.*.display_name
-    public_ip  = oci_core_instance.k8s_node.*.public_ip
-    private_ip = oci_core_instance.k8s_node.*.private_ip
+    name       = oci_core_instance.k8s_node[*].display_name
+    public_ip  = oci_core_instance.k8s_node[*].public_ip
+    private_ip = oci_core_instance.k8s_node[*].private_ip
   }
-  description = "names and IPs of created instances"
 }
 
 output "lb_public_ip" {
-  description = "public IPs of LB"
+  description = "Public IPs of LB"
   value       = oci_network_load_balancer_network_load_balancer.k8s_network_load_balancer.ip_addresses[0].ip_address
 }
 
@@ -58,11 +58,11 @@ output "lb_id" {
 }
 
 output "oci_ubuntu_images" {
-  description = "list of possible Ubuntu images"
+  description = "List of possible Ubuntu images"
   value = {
-    display_name             = data.oci_core_images.oci_ubuntu_images.images.*.display_name
-    operating_system         = data.oci_core_images.oci_ubuntu_images.images.*.operating_system
-    operating_system_version = data.oci_core_images.oci_ubuntu_images.images.*.operating_system_version
-    id                       = data.oci_core_images.oci_ubuntu_images.images.*.id
+    display_name             = data.oci_core_images.oci_ubuntu_images.images[*].display_name
+    operating_system         = data.oci_core_images.oci_ubuntu_images.images[*].operating_system
+    operating_system_version = data.oci_core_images.oci_ubuntu_images.images[*].operating_system_version
+    id                       = data.oci_core_images.oci_ubuntu_images.images[*].id
   }
 }
